3e262222c67b7ced77db734cf21b5f080155482f
JP3tri3 <57971582+JP3tri3@users.noreply.github.com>
2021-03-25 01:31:49 -0400

    SQL File fixes

--- sqltest.py
+++ sqltest.py
@@ -12,6 +12,8 @@ mycursor = db.cursor()
 
 # # create table:
 # mycursor.execute("CREATE TABLE Strategy (id VARCHAR(50), wt1 float UNSIGNED, wt2 float UNSIGNED, last_candle_high float UNSIGNED, last_candle_low float UNSIGNED, last_candle_vwap float UNSIGNED, active_position VARCHAR(50), new_trend VARCHAR(50), last_trend VARCHAR(50), active_trend VARCHAR(50))")
+# mycursor.execute("CREATE TABLE trade_values (id VARCHAR(16), symbol VARCHAR(16), symbol_pair VARCHAR(16), key_input INT UNSIGNED, limit_price_difference INT UNSIGNED, leverage INT UNSIGNED, input_quantity INT UNSIGNED, data_name VARCHAR(16)")
+
 
 # # describe table details:
 # mycursor.execute("DESCRIBE Strategy")
@@ -23,6 +25,8 @@ mycursor = db.cursor()
 # mycursor.execute("INSERT INTO Strategy () VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", ("30_min", 0.0, 0.0, 0.0, 0.0, 0.0, "null", "null", "null", "null"))
 # db.commit()
 
+mycursor.execute("INSERT INTO trades () VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", ("main", '', '', 0, 0.0, 0, 0, ''))
+db.commit()
 # mycursor.execute("SELECT * FROM Strategy")
 
 # Query
@@ -31,7 +35,7 @@ mycursor = db.cursor()
 #     print(x)
 
 # # Query specific
-# mycursor.execute("SELECT wt1, wt2 FROM Strategy WHERE name = '9_min'")
+# mycursor.execute("SELECT wt1, wt2 FROM Strategy WHERE id = '9_min'")
 
 # # Alter / Add
 # mycursor.execute("ALTER TABLE strategy ADD COLUMN test VARCHAR(50) NOT NULL")
@@ -43,9 +47,9 @@ mycursor = db.cursor()
 
 # mycursor.execute("ALTER TABLE Strategy CHANGE name id VARCHAR(50)")
 
-def updateDbValue(id_name, column_name, value):
+def updateTableValue(id_name, column_name, value):
     try:
-        query = "UPDATE strategy SET " + str(column_name) + "=" + str(value) + " WHERE id = '" + str(id_name) + "'" 
+        query = "UPDATE strategy SET " + str(column_name) + "=" + str(value) + " WHERE id = '" + str(id_name) + "'"
         print(query)
         mycursor.execute(query)
         db.commit()
@@ -53,18 +57,47 @@ def updateDbValue(id_name, column_name, value):
         print("Failed to update record to database: {}".format(error))
 
 
-def updateMultipleDbValues(id_name, wt1_value, wt2_value, last_candle_high_value, last_candle_low_value, last_candle_vwap_value):
+def updateStratValues(id_name, wt1, wt2, last_candle_high, last_candle_low, last_candle_vwap):
     try:
-        query = "UPDATE strategy SET wt1=" + str(wt1_value) + ", wt2=" + str(wt2_value) + ", last_candle_low=" + str(last_candle_low_value) + ", last_candle_high=" + str(last_candle_high_value) + ", last_candle_vwap=" + str(last_candle_vwap_value) + " WHERE id = '" + str(id_name) + "'"
+        query = "UPDATE strategy SET wt1=" + str(wt1) + ", wt2=" + str(wt2) + ", last_candle_low=" + str(last_candle_low) + ", last_candle_high=" + str(last_candle_high) + ", last_candle_vwap=" + str(last_candle_vwap) + " WHERE id = '" + str(id_name) + "'"
         print(query)
         mycursor.execute(query)
         db.commit()
     except mysql.connector.Error as error:
         print("Failed to update record to database: {}".format(error))
 
+def updateTradeValues(id_name, symbol, symbol_pair, key_input, limit_price_difference, leverage, input_quantity, data_name):
+    try:
+        query = "UPDATE trades SET symbol=" + str(symbol) + ", symbol_pair=" + str(symbol_pair) + ", key_input=" + str(key_input) + ", limit_price_difference=" + str(limit_price_difference) + ", leverage=" + str(leverage) + ", input_quantity=" + str(input_quantity) + ", data_name=" + str(data_name) + " WHERE id = '" + str(id_name) + "'"
+        print(query)
+        mycursor.execute(query)
+        db.commit()
+    except mysql.connector.Error as error:
+        print("Failed to update record to database: {}".format(error))       
 
+def viewDbValue(id_name, column_name):
+    try: 
+        query = "SELECT " + str(column_name) + " FROM strategy WHERE id = '" + str(id_name) + "'"
+        test = mycursor.execute(query)
+        result = mycursor.fetchall()
+        return result[0][0]
+    except mysql.connector.Error as error:
+        print("Failed to update record to database: {}".format(error))
+
+def clearAllTableValues():
+    updateTradeValues('main', 'empty', 'empty', 0, 0, 0, 0, 'empty')
+    updateStratValues('1_min', 0, 0, 0, 0, 0)
+    updateStratValues('9_min', 0, 0, 0, 0, 0)
+    updateStratValues('16_min', 0, 0, 0, 0, 0)
+    updateStratValues('30_min', 0, 0, 0, 0, 0)
 
 # updateDbValue('1_min', 'wt1', 0)
-updateMultipleDbValues('9_min', 0, 0, 0, 0, 0)
-
+# updateStratValues('9_min', 0, 0, 0, 0, 0)
+# viewDbValues('9_min', 'wt1')
 # mycursor.getUpdateCount()
+# print(viewDbValues('9_min', 'wt1'))
+# test1 = viewDbValues('9_min', 'wt1')
+
+# updateTradeValues('main', 'BTC', 'BTCUSD', 0, 0.5, 5, 500, 'test')
+# clearAllTableValues()
+
